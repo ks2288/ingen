c1caed3dccba5da2804305f6a807edd1baf3e126
Kenny Sofranko <kmsofranko@gmail.com>
2024-03-05 17:32:20 -0500

    stale file removal and a few script/directory def changes

--- src/main/kotlin/util/Constants.kt
+++ src/main/kotlin/util/Constants.kt
@@ -4,9 +4,9 @@ import kotlin.io.path.Path
 import kotlin.io.path.absolutePathString
 
 object CommandConstants {
-    private const val COMMANDS_FILE_NAME = "config/commands.json"
-    private const val CONFIG_FILE_NAME = "config/ingen.json"
     private val DEPLOYMENT_DIRECTORY = "${System.getProperty("user.home")}/.ingen"
+    private val CONFIG_FILE_NAME = "${DEPLOYMENT_DIRECTORY}/config/ingen.json"
+    private val COMMANDS_FILE_NAME = "${DEPLOYMENT_DIRECTORY}/config/commands.json"
     val COMMAND_SCHEMA_PATH =
         "$DEPLOYMENT_DIRECTORY/$COMMANDS_FILE_NAME"
     val CONFIG_FILE_PATH = "$DEPLOYMENT_DIRECTORY/$CONFIG_FILE_NAME"
